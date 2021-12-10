f94612981450be25c09bff16b412d4b5eb00b4c2
w0ng0wen <95466662+w0ng0wen@users.noreply.github.com>
2021-12-10 14:52:05 +0800

    personal

---
+++ data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-atrium_edited",
+      "name": "Atrium_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6435905797121642,
+          "pitch": -0.05752571963470032,
+          "rotation": 0,
+          "target": "1-level-2_edited"
+        },
+        {
+          "yaw": -0.046623162372370075,
+          "pitch": -0.04278468187349915,
+          "rotation": 0,
+          "target": "2-level-3_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6431117250013436,
+          "pitch": 0.07348131754736187,
+          "title": "Level 2",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.04022042234988987,
+          "pitch": 0.1326634017625068,
+          "title": "Level 3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-level-2_edited",
+      "name": "Level 2_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.0068083277785354,
+        "pitch": 0.004860458914111021,
+        "fov": 1.513843149153066
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0012967483993584,
+          "pitch": 0.012347493263572318,
+          "rotation": 3.141592653589793,
+          "target": "0-atrium_edited"
+        },
+        {
+          "yaw": 2.1580428693833893,
+          "pitch": 0.007452150877767849,
+          "rotation": 0,
+          "target": "2-level-3_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.992067155519768,
+          "pitch": 0.22295454515309387,
+          "title": "Atrium",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.164507513588065,
+          "pitch": 0.17047238692185118,
+          "title": "Level 3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-level-3_edited",
+      "name": "Level 3_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.133073502074316,
+        "pitch": 0.22467222788635866,
+        "fov": 1.513843149153066
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0183396404301135,
+          "pitch": -0.061735072799079305,
+          "rotation": 3.141592653589793,
+          "target": "0-atrium_edited"
+        },
+        {
+          "yaw": 2.495403272702971,
+          "pitch": -0.021028793947692748,
+          "rotation": 3.141592653589793,
+          "target": "1-level-2_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.025650616650376,
+          "pitch": 0.1120119700212463,
+          "title": "Atrium",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.484755580703787,
+          "pitch": 0.12676576395661243,
+          "title": "Level 2",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Business school",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
